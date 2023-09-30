fb5b31cdb5138cb787262f7e78e641de801f4943
Shogo Ogami <sg.tao.so@gmail.com>
2023-09-30 14:53:39 +0900

    update components/AioTyping for play audio files

--- src/components/AioTyping.jsx
+++ src/components/AioTyping.jsx
@@ -2,6 +2,10 @@
 import { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import ReactAudioPlayer from 'react-audio-player';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import StopCircleIcon from '@mui/icons-material/StopCircle';
 
 // eslint-disable-next-line react/prop-types
 const AioTyping = ({ contents = {} }) => {
@@ -9,6 +13,11 @@ const AioTyping = ({ contents = {} }) => {
     let [typeSlashed, setTypeSlashed] = useState(true);
     let [englishText, setEnglishText] = useState("");
     let [translationSlashed, setTranslationSlashed] = useState("");
+
+    const baseAudioUrl = "/assets/Natural_419"
+    let [audioUrl, setAudioUrl] = useState(`${baseAudioUrl}/001.mp3`);
+    let [isAudioPlaying, setAudioPlaying] = useState(false);
+
     const updateTexts = (index) => {
         setEnglishText(contents.items[index].englishText);
         if (typeSlashed)
@@ -16,9 +25,36 @@ const AioTyping = ({ contents = {} }) => {
         else
             setTranslationSlashed(contents.items[index].translation_natural);
     }
+
+    const updateAudioUrl = (baseAudioUrl, audioNo) => {
+        let audioFile = audioNo.toString().padStart(3, '0') + ".mp3";
+        console.log(`set audio of ${audioFile}`);
+        setAudioUrl(`${baseAudioUrl}/${audioFile}`);
+        setAudioPlaying(true); // 自動再生する
+    }
+    const AudioPlayer = (porps) => {
+        const handleTogglePlay = () => {
+            porps.setPlaying((prev) => !prev);
+        };
+        return (
+            <div>
+                <ReactAudioPlayer
+                    src={porps.audioSrc}
+                    autoPlay={porps.isPlaying}
+                    controls={false}
+                />
+                <Button variant="contained" onClick={handleTogglePlay}>
+                    {porps.isPlaying ?
+                        (<><StopCircleIcon /> Stop</>) :
+                        (<><PlayCircleOutlineIcon /> Play</>)
+                    }
+                </Button>
+            </div>
+        );
+    };
+
     useEffect(() => {
         console.log('useEffect is called');
-        updateTexts(itemIndex);
         // イベントリスナーの設定
         const handleKeyDown = (event) => {
             if (event.key === 'ArrowRight') {
@@ -31,13 +67,24 @@ const AioTyping = ({ contents = {} }) => {
                 console.log('Tabキーが押されました！');
                 setTypeSlashed(prev => !prev);
                 event.preventDefault(); // デフォルトの動作を防ぐ
+            } else if (event.key === ' ') {
+                // SPACEキーでオーディオ再生をトグル
+                console.log('SPACEキーが押されました！');
+                setAudioPlaying((prev) => !prev);
             }
         };
+
         // イベントリスナーを追加
         window.addEventListener('keydown', handleKeyDown);
 
+        // 表示テキストを設定
+        updateTexts(0);
+        // オーディオファイルパスを設定
+        updateAudioUrl(baseAudioUrl, contents.items[0].no);
+
         // コンポーネントがアンマウントされた時にイベントリスナーを解除
         return () => {
+            setAudioPlaying(false);
             window.removeEventListener('keydown', handleKeyDown);
         };
     }, [contents]);
@@ -45,6 +92,13 @@ const AioTyping = ({ contents = {} }) => {
     useEffect(() => {
         console.log(`itemIndex is ${itemIndex}`);
         updateTexts(itemIndex);
+        // オーディオファイルパスを設定
+        updateAudioUrl(baseAudioUrl, contents.items[itemIndex].no);
+
+        // アイテム切り替え時は再生停止
+        return () => {
+            setAudioPlaying(false);
+        };
     }, [itemIndex, typeSlashed]);
 
     return (
@@ -59,8 +113,8 @@ const AioTyping = ({ contents = {} }) => {
             <Typography variant="h6" gutterBottom>
                 {contents.category_name}（{(itemIndex + 1)}／{(contents.items.length)}）
             </Typography>
-            <Typography align="center" sx={{ color: 'red' }}>
-                [Tab]=(訳語切替)、[右Key]=(次へ)、[左Key]=(前へ)
+            <Typography variant="body2" align="center" sx={{ color: 'red' }}>
+                [SPACE]=(再生), [Tab]=(訳語切替), [右Key]=(次へ), [左Key]=(前へ)
             </Typography>
 
             <Box mt={2} fontWeight={"bold"} >
@@ -71,12 +125,19 @@ const AioTyping = ({ contents = {} }) => {
             </Typography>
 
             <Box mt={2} fontWeight={"bold"} >
-                訳文（{ (typeSlashed) ? "語順訳" : "和訳"}）：
+                訳文（{(typeSlashed) ? "語順訳" : "和訳"}）：
             </Box>
             <Typography variant="bodyTranslation" gutterBottom>
                 {translationSlashed}
             </Typography>
-            <Box mt={2}></Box>
+
+            <Box align="center" mt={4} mb={4} >
+                <AudioPlayer
+                    audioSrc={audioUrl}
+                    isPlaying={isAudioPlaying}
+                    setPlaying={setAudioPlaying}
+                />
+            </Box>
         </Box>
     );
 }
